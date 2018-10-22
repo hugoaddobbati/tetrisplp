981f6850d80bb7a4103637cf688ec00cff88c07c
hugo galvao <hugo.galvao@ccc.ufcg.edu.br>
2018-10-22 15:04:07 -0300

    adding basic data structures
    Co-authored-by: Arthur Silva <arthur.silva.rodrigues@ccc.ufcg.edu.br>
    Co-authored-by: Felipe Mota <felipe.santos@ccc.ufcg.edu.br>
    Co-authored-by: Jose Vinicius <jose.arruda@ccc.ufcg.edu.br>
    Co-authored-by: Katson Matheus <katson.alves@ccc.ufcg.edu.br>

--- haskell/tetris.hs
+++ haskell/tetris.hs
@@ -5,5 +5,26 @@
 module Main where
 import System.IO
 
+data Point = 
+    Point {
+        _x :: Int,
+        _y :: Int,
+        _color :: [Char]
+    }
+
+data Board = 
+    Board {
+        _tetrominoPts :: [Point],
+        _points :: [Point],
+        _score :: Int,
+        _powerUp :: Int
+    }
+
+data St =
+    St {_board :: Board,
+        _g :: (Int, StdGen),
+        _lastBestScore :: Int
+       }
+
 main :: IO ()
 main = do putStrLn $ "it Works"
