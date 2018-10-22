df8eb4d024940c28a36412b89f9d1c5246f70941
hugo galvao <hugo.galvao@ccc.ufcg.edu.br>
2018-10-22 16:01:20 -0300

    creating initial appstate, color map, event handler, without defining functions
    Co-authored-by: Arthur Silva <arthur.silva.rodrigues@ccc.ufcg.edu.br>
    Co-authored-by: Felipe Mota <felipe.santos@ccc.ufcg.edu.br>
    Co-authored-by: Jose Vinicius <jose.arruda@ccc.ufcg.edu.br>
    Co-authored-by: Katson Matheus <katson.alves@ccc.ufcg.edu.br>

--- haskell/tetris.hs
+++ haskell/tetris.hs
@@ -69,5 +69,68 @@ data St =
         _lastBestScore :: Int
        }
 
+theApp :: App St CustomEvent ()
+theApp =
+    App { appDraw = drawUI
+        , appChooseCursor = showFirstCursor
+        , appHandleEvent = appEvent
+        , appStartEvent = return
+        , appAttrMap = const theMap
+        }
+
+appEvent :: St -> BrickEvent () CustomEvent -> EventM () (Next St)
+appEvent st e =
+    case e of
+        VtyEvent (V.EvKey (V.KChar 'p') []) -> powerUpSt st
+        VtyEvent (V.EvKey V.KEsc []) -> halt st
+        VtyEvent (V.EvKey V.KRight []) -> goRightTetromino st
+        VtyEvent (V.EvKey V.KLeft []) -> goLeftTetromino st  
+        VtyEvent (V.EvKey V.KUp []) -> rotateTetrominoSt st  
+        VtyEvent (V.EvKey V.KDown []) -> tryGoDownTetromino st
+        VtyEvent (V.EvKey (V.KChar ' ') []) -> pushDownTetrominoEvent st  
+        VtyEvent _ -> continue st
+        AppEvent Counter -> tryGoDownTetromino st
+        _ -> continue st
+
+globalDefault :: V.Attr
+globalDefault = V.white `on` V.black
+
+
+theMap :: AttrMap
+theMap = attrMap globalDefault
+    [ 
+        ("general",          V.black `on` V.black),
+        ("readyStatus",      V.green `on` V.black),
+        ("notReadyStatus",   V.red `on` V.black),
+        ("T",                V.blue `on` V.blue),
+        ("Z",                V.white `on` V.white),
+        ("I",                V.yellow `on` V.yellow),
+        ("L",                V.magenta `on` V.magenta),
+        ("J",                V.cyan `on` V.cyan),
+        ("S",                V.red `on` V.red),
+        ("O",                V.green `on` V.green)
+    ]
+
+initBoard :: Board
+initBoard = Board getRandomTetromino [] 0 0
+
+playGame :: Int -> IO St
+playGame a = do
+    chan <- newBChan 10
+
+    forkIO $ forever $ do
+        threadDelay 200000
+        writeBChan chan Counter
+
+    game <- customMain (V.mkVty V.defaultConfig) (Just chan) theApp (initialState a)
+    return game
+
+getScore :: St -> Int
+getScore st = sc
+    where
+        bd = st^.board
+        sc = bd^.score
+
+
 main :: IO ()
 main = do putStrLn $ "it Works"
